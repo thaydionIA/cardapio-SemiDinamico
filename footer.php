6fb7503a732d49abbfa4b61e9c9ba29f2525ba3b
thaydionIA <thaydion.ia@gmail.com>
2024-10-21 10:23:38 -0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+<?php include 'config.php'; ?>
+</main>
+<link rel="stylesheet" href="assets/css/style.css">
+<footer>
+        <p>&copy; 2024 <?php echo $site_name; ?>. Todos os direitos reservados.</p>
+        <li><a href="admin/login.php">Painel Administrativo</a></li>
+    </footer>
+</body>
+</html>
